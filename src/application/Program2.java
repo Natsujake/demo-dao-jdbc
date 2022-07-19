4c58410d8c3a8b1c2c81bf2cd5c2385409376f40
Natsujake <75273434+Natsujake@users.noreply.github.com>
2022-07-19 11:47:10 -0300

    Department Insert Implamentation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -26,6 +26,14 @@ public class Program2 {
 			System.out.println(obj);
 		}
 
+		System.out.println("\n=== TEST 3: Department Insert ===");
+		Department newDepartment = new Department(null, "Games");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id= " + newDepartment.getId());
+		
+		
+		
+		
 	}
 
 }
