e4ac8d0450bfdae70dc98834c98797973405686d
Natsujake <75273434+Natsujake@users.noreply.github.com>
2022-07-19 10:23:43 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,9 +36,15 @@ public class Program {
 		System.out.println("\n=== TEST 4: Sellers Insert ===");
 		Sellers newSeller = new Sellers(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
 		sellersDao.insert(newSeller);
-		
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 		
+		
+		System.out.println("\n=== TEST 5: Sellers Update ===");
+		sellers = sellersDao.findById(1);
+		sellers.setName("Martha Wayne");
+		sellers.setEmail("MarthaWayne@gmail.com");
+		sellersDao.update(sellers);
+		System.out.println("Update completed " + sellers);
 		
 	}
 }
